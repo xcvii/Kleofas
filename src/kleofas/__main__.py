fea62630a22fd817080d0f301bad07a54c088005
Endre Tamas SAJO <sajobandi@gmail.com>
2017-03-27 22:19:39 +0100

    add help text

--- src/kleofas/__main__.py
+++ src/kleofas/__main__.py
@@ -17,7 +17,6 @@ logging.basicConfig(format='%(asctime)-12s %(levelname)-5s %(name)s: %(message)s
 def __parse_rcfile(path):
     try:
         with open(path, 'r') as fh:
-            logging.info("getting extra flags from %s" % path)
             return list(fh.read().split())
     except sys.FileNotFoundError:
         return list()
@@ -29,16 +28,18 @@ def main():
     import os.path
     import asyncio
 
-    logging.info("running %s version %s" % (__project__, __version__))
-
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--token',  required=True, help='Telegram bot token')
     parser.add_argument('--owner', help='Only respond to this user id when specified')
     parser.add_argument('--no-owner', dest='owner', action='store_const', const=None)
-    parser.add_argument('--command-file', help='')
+    parser.add_argument('--command-file', metavar='PATH', help='Specify a python file with global \
+            cmd_* functions to be used as custom commands')
 
     rc_file = os.path.expanduser(os.getenv('KLEOFASRC') or '~/.kleofasrc')
     args = parser.parse_args(__parse_rcfile(rc_file) + sys.argv[1:])
+
+    logging.info("running %s version %s" % (__project__, __version__))
+    logging.info("getting extra flags from %s" % rc_file)
 
     censored_args = { k: '...' if k == 'token' else vars(args)[k] for k in vars(args) }
     logging.info("using options: %s" % censored_args)
