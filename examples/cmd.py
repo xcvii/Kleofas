5733ef3435ed4deb6ba9fc1554615510cafd9f13
Endre Tamas SAJO <sajobandi@gmail.com>
2017-03-28 08:28:34 +0100

    add /fortune command

--- examples/cmd.py
+++ examples/cmd.py
@@ -2,11 +2,17 @@
 '''
 
 
-import time
-
-
 def cmd_xyzzy():
     '''Say the magic word'''
+
+    import time
     time.sleep(3)
     return 'Nothing happens.'
+
+
+def cmd_fortune():
+    '''"Print a random, hopefully interesting, adage"'''
+
+    import subprocess
+    return subprocess.check_output(['fortune'])
 
